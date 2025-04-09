6601a6c7fd9e7224c3cd3861e6fcd12516efbf45
ahmahmmedd <117048252+ahmahmmedd@users.noreply.github.com>
2025-04-10 03:57:55 +0530

    Delete entity directory

--- entity/user.py
+++
@@ -1,6 +0,0 @@
-class User:
-    def __init__(self, user_id, username, password, role):
-        self.user_id = user_id
-        self.username = username
-        self.password = password
-        self.role = role  # "Admin" or "User"
